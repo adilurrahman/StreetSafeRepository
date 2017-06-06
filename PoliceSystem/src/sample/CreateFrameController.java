8af12c01fc46bbefe9c68643a0c0ad1643b67e5d
Maleesh Dilshan Piyaratne <maleesh.2015053@iit.ac.lk>
2017-06-06 14:59:47 +0530

    GaveIDs
    sjvnlj

---
+++ PoliceSystem/src/sample/CreateFrameController.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by Maleesh Dilshan on 6/6/2017.
+ */
+public class CreateFrameController {
+}
--- PoliceSystem/src/sample/Main.java
+++ PoliceSystem/src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
         primaryStage.setTitle("Street Safe");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
